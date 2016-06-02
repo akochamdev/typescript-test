a9123cc2df9f7f917619d8a98810c2667eaa27af
Charles Diakoparaskevas <Charles Diakoparaskevas>
2016-06-02 01:11:20 -0400

    Added tsconfig and task builder.  Upgraded tsc on Windows so it runs.

--- greeter.js
+++ greeter.js
@@ -6,12 +6,11 @@ var Student = (function () {
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
     return Student;
-})();
-
+}());
+// This function displays the word Hello followed by the passed in person's first and last name
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-
 var user = new Student("Jane", "M.", "User");
-
 document.body.innerHTML = greeter(user);
+//# sourceMappingURL=greeter.js.map
