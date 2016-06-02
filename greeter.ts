51afe322d4d66f2889c034250931615841d85545
Charles Diakoparaskevas <akochamdev@gmail.com>
2016-06-01 21:52:01 -0400

    Added comment for greeter message

--- greeter.ts
+++ greeter.ts
@@ -10,6 +10,7 @@ interface Person {
     lastName: string;
 }
 
+// This function displays the word Hello followed by the passed in person's first and last name
 function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
